ebcd70c775becb81284da4af888afc5a35b2a245
tolgakb <tolgakabaahmet@gmail.com>
2023-03-31 15:27:35 +0100

    Cars app has been created, car model has been created and featured and latest car are listed at homepage regarding to data in the model

--- carzone/settings.py
+++ carzone/settings.py
@@ -36,12 +36,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages',
+    'cars',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
